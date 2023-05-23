7b4d652f3465a32adf47ad1485af47f61d7b8f02
Sebastien Deschamps <sdx007.com@gmail.com>
2023-05-23 07:26:15 +0200

    Backup during adding eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+// .eslintrc.js example
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2022": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+}
